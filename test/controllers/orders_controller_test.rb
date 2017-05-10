5d7dbaf26b8bdac33c6ee441a27c3477ecb109a6
kicohen <kicohen@andrew.cmu.edu>
2017-05-10 02:55:15 -0400

    Removed controller tests.

--- test/controllers/orders_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OrdersControllerTest < ActionController::TestCase
-  test "should get mark_item_as_shipped" do
-    get :mark_item_as_shipped
-    assert_response :success
-  end
-
-end
--- test/controllers/stores_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StoresControllerTest < ActionController::TestCase
-  test "should get remove_from_cart" do
-    get :remove_from_cart
-    assert_response :success
-  end
-
-end
